ee68841d3835133a844a806807903ad9119d708e
Arlin-Mafra <arlinsantana@gmail.com>
2022-07-22 08:57:01 -0300

    mentoria e curso

--- desafio_poo/src/App.java
+++ desafio_poo/src/App.java
@@ -1,5 +1,17 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.modelo.Curso;
+import br.com.dio.desafio.modelo.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Curso curso = new Curso();
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
     }
 }
